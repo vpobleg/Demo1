27a1bc87eb4964b10240f11663332f554dc5967f
vpobleg <poblete.victor@gmail.com>
2020-11-05 21:37:28 -0300

    feature Suma/Resta

--- holaMundo.java
+++ holaMundo.java
@@ -1,5 +1,6 @@
 public class holaMundo{
 	public static void main(String[] args){
-		System.out.println("Hola Devops");
+		System.out.println("Calculadora--Suma");
+		System.out.println("Calculadora--Resta");
 	}
 }
