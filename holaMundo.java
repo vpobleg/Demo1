9873002afb62237dcb2fc1e784be85e08e5f2c70
vpobleg <poblete.victor@gmail.com>
2020-11-05 21:48:54 -0300

    Se agrega feature de multiplicacion

--- holaMundo.java
+++ holaMundo.java
@@ -1,6 +1,5 @@
 public class holaMundo{
 	public static void main(String[] args){
-		System.out.println("Calculadora--Suma");
-		System.out.println("Calculadora--Resta");
+		System.out.println("Calculadora--Multiplicacion");
 	}
 }
